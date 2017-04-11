719c97da92d07361804bcbb650ca8ef8124eebae
Sana Jahan <sanadgrt@gmail.com>
2017-04-11 21:59:59 +0530

    Fixed error

--- serverController.js
+++ serverController.js
@@ -94,7 +94,7 @@ exports.stopServer = function(fromId){
  // CHECK RUNNING PROCESSES
 
 exports.checkRunningProcesses = function(fromId) {
-			cmd = 'ps -a command,%cpu,%mem';
+			cmd = 'ps -e command,%cpu,%mem';
 		exec(cmd, function(err, stdout, stderr) {
 			    if (err) {
 			        bot.sendMessage(fromId,"\n"+stderr);
